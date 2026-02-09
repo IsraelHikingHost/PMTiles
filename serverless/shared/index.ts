a514641d303b4bfc93bd871a98fc501d9be1a03c
zstadler <zeev.stadler@gmail.com>
2026-02-09 10:04:31 +0200

    Allow `+` in source names

--- serverless/shared/index.ts
+++ serverless/shared/index.ts
@@ -13,9 +13,9 @@ export const pmtiles_path = (name: string, setting?: string): string => {
 };
 
 const TILE =
-  /^\/(?<NAME>[0-9a-zA-Z\/!\-_\.\*\'\(\)]+)\/(?<Z>\d+)\/(?<X>\d+)\/(?<Y>\d+).(?<EXT>[a-z]+)$/;
+  /^\/(?<NAME>[0-9a-zA-Z\/!\-_\.\*\'\(\)+]+)\/(?<Z>\d+)\/(?<X>\d+)\/(?<Y>\d+).(?<EXT>[a-z]+)$/;
 
-const TILESET = /^\/(?<NAME>[0-9a-zA-Z\/!\-_\.\*\'\(\)]+).json$/;
+const TILESET = /^\/(?<NAME>[0-9a-zA-Z\/!\-_\.\*\'\(\)+]+).json$/;
 
 export const tile_path = (path: string): SliceInput => {
   const tile_match = path.match(TILE);
